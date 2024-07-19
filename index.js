11a60ab9d4b994efc73fc057c41937b8d965bf82
Alvaro Zacarias <alvaro.zacmat@gmail.com>
2024-07-18 22:58:47 -0600

    A bit more

--- index.js
+++ index.js
@@ -39,3 +39,33 @@ subButtons.forEach(button => {
     })
 })
 
+// Validate form
+
+const emailInput = document.getElementById('join-form');
+const errorMessage = document.getElementById('error-message');
+const form = document.getElementById('form');
+
+function handleSubmit(e) {
+    e.preventDefault(e);
+  
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+  
+    console.log(data);
+
+    for (const [key, value] of Object.entries(data)) {    
+        console.log(`Key: ${key}, Value: ${value}`);
+        const validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+        if (email.value.match(validEmail)) {
+            emailInput.style = '';
+            errorMessage.innerHTML = ''; // HACER QUE HAGA ALGO CUANDO ESTÉ BIEN
+        } else if (email.value === '' || !validEmail.test(email)) {
+            errorMessage.style.display = 'block';
+            emailInput.style.outline = "#FFF"
+        }; 
+    };
+}
+
+form.addEventListener("submit", handleSubmit);
+
+
